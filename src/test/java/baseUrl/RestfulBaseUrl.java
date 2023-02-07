8206de271485e883c841005eca410aaa0bcf492c
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2023-02-07 14:38:39 +0100

    Add files via upload

---
+++ src/test/java/baseUrl/RestfulBaseUrl.java
@@ -0,0 +1,15 @@
+package baseUrl;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class RestfulBaseUrl {
+
+    protected RequestSpecification spec01;
+
+    @Before
+    public void setUp() {
+        spec01 = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com/").build();
+    }
+}
